73e18e952ab19e44132687b7fd8aa6a35f50c719
Diego Pérez V <diego@flen.com.ec>
2022-01-18 15:30:50 -0500

    Login screen added

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -17,9 +17,8 @@ class _LoginScreenState extends State<LoginScreen> {
           width: double.infinity,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
+            children: const [
               Text('LOGO', style: TextStyle( fontSize: 30.0)),
-              
             ],
           ),
         ),
